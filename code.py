56f8a3145e324463fbd0ac6906745e080bf55fbd
Nagaraju Kallepalli <anuuppalapati90@gmail.com>
2018-10-07 17:07:59 +0530

    copy file

---
+++ code.py
@@ -0,0 +1,19 @@
+import pandas as pd
+from openpyxl import load_workbook
+from openpyxl.utils import get_column_letter
+
+files= load_workbook(filename='excel2.xlsx',read_only=False)
+print files
+class Profiles(input_sheet,output)
+    self.input_sheet=input_sheet
+    self.output = output
+    return obj
+
+def mainfn():
+    data = Profiles(inputs,outputs)
+inputs='excel1.xlsx'
+input_sheet = inputs.loc[inputs['particulars']=='input',reference].iloc[0]
+print input_sheet
+
+
+mainfn('excel1.xlsx','excel2.xlsx')
